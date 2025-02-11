1c8ee6a258f35942a19d207c1b57ef6d5a34aa6f
YOGESHWARAN D <124228144+YOGESHWARAN3645@users.noreply.github.com>
2025-02-11 11:03:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-def add(a, b):
-    return a + b
-
-def subtract(a, b):
-    return a - b
-
-if __name__ == "__main__":
-    print("Add 2 + 3:", add(2, 3))
-    print("Subtract 5 - 2:", subtract(5, 2))
+# main.py
+def some_function():
+    return "Expected Output"
